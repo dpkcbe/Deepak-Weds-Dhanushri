b9e39f0909a2b79001b5c442e8f38b936a07dade
dpkcbe <dpkcbe@gmail.com>
2026-07-24 20:56:28 +0530

    Remove scroll indicator mouse icon

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -184,21 +184,6 @@ export default function HeroSection() {
           </span>
         </motion.button>
 
-        {/* Scroll indicator */}
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 2.5 }}
-          className="absolute bottom-8 left-1/2 -translate-x-1/2"
-        >
-          <motion.div
-            animate={{ y: [0, 8, 0] }}
-            transition={{ duration: 2, repeat: Infinity }}
-            className="w-6 h-10 rounded-full border-2 border-gold/30 flex items-start justify-center p-1"
-          >
-            <div className="w-1 h-2 rounded-full bg-gold/50" />
-          </motion.div>
-        </motion.div>
       </div>
     </section>
   );
